ad9c5d5cea59c1b93959ddcb7aea49e27b612b30
vjaju20 <varun0120.cse19@chitkara.edu.in>
2021-12-31 14:31:06 +0530

    Add all my files

---
+++ payment.js
@@ -0,0 +1,120 @@
+function totalAmount(){
+	//French Cuisine
+	var a1 = document.getElementById("aa1").value;
+	var b1 = document.getElementById("bb1").value;
+	var c1 = document.getElementById("cc1").value;
+	var d1 = document.getElementById("dd1").value;
+	var e1 = document.getElementById("ee1").value;
+	var f1 = document.getElementById("ff1").value;
+	var g1 = document.getElementById("gg1").value;
+	var h1 = document.getElementById("hh1").value;
+	var i1 = document.getElementById("ii1").value;
+	var j1 = document.getElementById("jj1").value;
+	var k1 = document.getElementById("kk1").value;
+	var l1 = document.getElementById("ll1").value;
+	var m1 = document.getElementById("mm1").value;
+	var n1 = document.getElementById("nn1").value;
+	var o1 = document.getElementById("oo1").value;
+	var p1 = document.getElementById("pp1").value;
+	var q1 = document.getElementById("qq1").value;
+	var r1 = document.getElementById("rr1").value;
+	var s1 = document.getElementById("ss1").value;
+	var t1 = document.getElementById("tt1").value;
+	
+	//Italian Cuisine
+	var a2 = document.getElementById("aa2").value;
+	var b2 = document.getElementById("bb2").value;
+	var c2 = document.getElementById("cc2").value;
+	var d2 = document.getElementById("dd2").value;
+	var e2 = document.getElementById("ee2").value;
+	var f2 = document.getElementById("ff2").value;
+	var g2 = document.getElementById("gg2").value;
+	var h2 = document.getElementById("hh2").value;
+	var i2 = document.getElementById("ii2").value;
+	var j2 = document.getElementById("jj2").value;
+	var k2 = document.getElementById("kk2").value;
+	var l2 = document.getElementById("ll2").value;
+	var m2 = document.getElementById("mm2").value;
+	var n2 = document.getElementById("nn2").value;
+	var o2 = document.getElementById("oo2").value;
+	var p2 = document.getElementById("pp2").value;
+	var q2 = document.getElementById("qq2").value;
+	var r2 = document.getElementById("rr2").value;
+	var s2 = document.getElementById("ss2").value;
+	var t2 = document.getElementById("tt2").value; 
+	
+	//Indian Cuisine
+	var a3 = document.getElementById("aa3").value;
+	var b3 = document.getElementById("bb3").value;
+	var c3 = document.getElementById("cc3").value;
+	var d3 = document.getElementById("dd3").value;
+	var e3 = document.getElementById("ee3").value;
+	var f3 = document.getElementById("ff3").value;
+	var g3 = document.getElementById("gg3").value;
+	var h3 = document.getElementById("hh3").value;
+	var i3 = document.getElementById("ii3").value;
+	var j3 = document.getElementById("jj3").value;
+	var k3 = document.getElementById("kk3").value;
+	var l3 = document.getElementById("ll3").value;
+	var m3 = document.getElementById("mm3").value;
+	var n3 = document.getElementById("nn3").value;
+	var o3 = document.getElementById("oo3").value;
+	var p3 = document.getElementById("pp3").value;
+	var q3 = document.getElementById("qq3").value;
+	var r3 = document.getElementById("rr3").value;
+	var s3 = document.getElementById("ss3").value;
+	var t3 = document.getElementById("tt3").value;
+	
+	//Japanese Cuisine
+	var a4 = document.getElementById("aa4").value;
+	var b4 = document.getElementById("bb4").value;
+	var c4 = document.getElementById("cc4").value;
+	var d4 = document.getElementById("dd4").value;
+	var e4 = document.getElementById("ee4").value;
+	var f4 = document.getElementById("ff4").value;
+	var g4 = document.getElementById("gg4").value;
+	var h4 = document.getElementById("hh4").value;
+	var i4 = document.getElementById("ii4").value;
+	var j4 = document.getElementById("jj4").value;
+	var k4 = document.getElementById("kk4").value;
+	var l4 = document.getElementById("ll4").value;
+	var m4 = document.getElementById("mm4").value;
+	var n4 = document.getElementById("nn4").value;
+	var o4 = document.getElementById("oo4").value;
+	var p4 = document.getElementById("pp4").value;
+	var q4 = document.getElementById("qq4").value;
+	var r4 = document.getElementById("rr4").value;
+	var s4 = document.getElementById("ss4").value;
+	var t4 = document.getElementById("tt4").value;
+	
+	//Australian Cuisine
+	var a5 = document.getElementById("aa5").value;
+	var b5 = document.getElementById("bb5").value;
+	var c5 = document.getElementById("cc5").value;
+	var d5 = document.getElementById("dd5").value;
+	var e5 = document.getElementById("ee5").value;
+	var f5 = document.getElementById("ff5").value;
+	var g5 = document.getElementById("gg5").value;
+	var h5 = document.getElementById("hh5").value;
+	var i5 = document.getElementById("ii5").value;
+	var j5 = document.getElementById("jj5").value;
+	var k5 = document.getElementById("kk5").value;
+	var l5 = document.getElementById("ll5").value;
+	var m5 = document.getElementById("mm5").value;
+	var n5 = document.getElementById("nn5").value;
+	var o5 = document.getElementById("oo5").value;
+	var p5 = document.getElementById("pp5").value;
+	var q5 = document.getElementById("qq5").value;
+	var r5 = document.getElementById("rr5").value;
+	var s5 = document.getElementById("ss5").value;
+	var t5 = document.getElementById("tt5").value;
+	
+	var tamount = (a1*80) + (b1*70) + (c1*100) + (d1*130) + (e1*70) + (f1*230) + (g1*165) + (h1*225) + (i1*270) + (j1*255) + (k1*240) + (l1*220) + (m1*300) + (n1*285) + (o1*190) + (p1*110) + (q1*225) + (r1*155) + (s1*90) + (t1*140)
+	              + (a2*125) + (b2*140) + (c2*100) + (d2*115) + (e2*130) + (f2*170) + (g2*215) + (h2*195) + (i2*230) + (j2*240) + (k2*265) + (l2*210) + (m2*175) + (n2*300) + (o2*270) + (p2*140) + (q2*65) + (r2*80) + (s2*90) + (t2*125)
+				  + (a3*40) + (b3*20) + (c3*30) + (d3*170) + (e3*150) + (f3*220) + (g3*180) + (h3*270) + (i3*175) + (j3*205) + (k3*250) + (l3*240) + (m3*225) + (n3*330) + (o3*295) + (p3*20) + (q3*25) + (r3*35) + (s3*60) + (t3*45)
+				  + (a4*135) + (b4*110) + (c4*120) + (d4*85) + (e4*100) + (f4*195) + (g4*210) + (h4*165) + (i4*150) + (j4*230) + (k4*275) + (l4*180) + (m4*240) + (n4*225) + (o4*280) + (p4*130) + (q4*145) + (r4*95) + (s4*150) + (t4*75)
+				  + (a5*125) + (b5*140) + (c5*160) + (d5*130) + (e5*175) + (f5*170) + (g5*220) + (h5*265) + (i5*185) + (j5*150) + (k5*245) + (l5*230) + (m5*165) + (n5*210) + (o5*285) + (p5*75) + (q5*90) + (r5*140) + (s5*155) + (t5*120);
+				  
+	document.getElementById("ta").innerHTML = "Your total Bill is Rs. " + tamount;
+	localStorage.setItem("bill",tamount.toString());
+}
